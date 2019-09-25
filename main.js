8527bd79c338e5b79ab05dce78ff5a6aa8436267
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-09-25 14:15:49 +0200

    Loads options depending on how many questions exist inside of array.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   createQuizDivElement = document.getElementById("createQuiz");
   createQuizButtonElement = document.getElementById("btn_CreateQuiz");
-  const quizArray = ["question 1", "question 2", "question 3", "question 4", "question 5", "question 6"];
+  const questionArray = ["Vem är djungelns konung?", "Vem är Zlatan", "Vad heter Elon Musks huvudföretag", "Vad är javascript?", "vad är ett objekt?"];
+  select_question = document.getElementById("select_question");
+  for (var questionValue = 1; questionValue < questionArray.length + 1; questionValue++) {
+    select_question.insertAdjacentHTML("beforeend", "<option>" + questionValue + "</option>");
+  }
 
   class Quiz {
     constructor() {
-      this.userName = createQuizUserNameValue; //Should be a string
-      this.noOfQuestions = quizArray.length; //Should be an array length.
+      this.userName = userNameValue; //Should be a string
+      this.noOfQuestions = questionArray.length; //Should be an array length.
       this.noOfRightAnswers = 0; //Should be a number/array
       this.noOfWrongAnswers = 0; //Should be a number/array
     }
@@ -14,22 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
   class Question {
     constructor() {
       this.questionCategory = ""; //Should be a string
-      this.question = ""; //Should be a string
+      this.question = questionArray; //Should be a string
       this.questionAnswers = []; //Should be an array
       this.isQuestionRightOrWrong = true; //Should be bool
     }
   }
   document.getElementById('btn_CreateQuiz').addEventListener("click", () => {
-    createQuizUserNameValue = document.getElementById("userNameInput").value;
+    userNameValue = document.getElementById("userNameInput").value;
     let quiz = new Quiz();
-    console.log(quiz.userName + quiz.noOfQuestions);
+    let question = new Question();
     selectValue = document.getElementById("select_question").value;
-    createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'><p>this is the form</p></div></div>");
+    createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'></div></div>");
     let questionDiv = document.getElementById("questionsDiv");
     questionDiv.parentNode.removeChild(questionDiv);
     let quizFormElement = document.getElementById("quizForm");
     for (var quizQuestionIndex = 0; quizQuestionIndex < selectValue; quizQuestionIndex++) {
-      quizFormElement.insertAdjacentHTML("beforeend", "<div class='centerDiv'><span>" + quizArray[quizQuestionIndex] + "</span></div>");
+      quizFormElement.insertAdjacentHTML("beforeend", "<div class='centerDiv'><span>" + question.question[quizQuestionIndex] + "</span></div>");
     }
   });
 });
