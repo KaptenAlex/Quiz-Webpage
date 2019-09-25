101f7c62b563b67fde1fe101864d5c111280ae76
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-09-25 10:02:43 +0200

    removed id in index.html, added more tags to be created and removed css.

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   createQuizButtonElement = document.getElementById("btn_CreateQuiz");
   document.getElementById('btn_CreateQuiz').addEventListener("click", () => {
     console.log("hej");
-    createQuizDivElement.insertAdjacentHTML("afterend", "<div id='quizForm' class='d-flex justify-content-center'></div>");
+    createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'><p>hello im a teapot</p></div></div>");
     createQuizButtonElement.disabled = "disabled";
-
   });
 });
