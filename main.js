28779f4e7b14ec231968bbe15ef4d84d7b44563d
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-09-25 09:49:13 +0200

    btn_CreateQuiz now creates a new div at the bottom of the body

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", () => {
+  createQuizDivElement = document.getElementById("createQuiz");
+  createQuizButtonElement = document.getElementById("btn_CreateQuiz");
+  document.getElementById('btn_CreateQuiz').addEventListener("click", () => {
+    console.log("hej");
+    createQuizDivElement.insertAdjacentHTML("afterend", "<div id='quizForm' class='d-flex justify-content-center'></div>");
+    createQuizButtonElement.disabled = "disabled";
 
+  });
 });
