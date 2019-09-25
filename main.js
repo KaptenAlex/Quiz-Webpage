1a516825448381b02914ca0efe6f92308658e318
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-09-25 10:28:18 +0200

    Prints out questions depending on the length of quiz array.

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   createQuizDivElement = document.getElementById("createQuiz");
   createQuizButtonElement = document.getElementById("btn_CreateQuiz");
+  const quizArray = ["question 1", "question 2", "question 3", "question 4", "question 5", "question 6"];
   document.getElementById('btn_CreateQuiz').addEventListener("click", () => {
-    console.log("hej");
     createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'><p>this is the form</p></div></div>");
-    var questionDiv = document.getElementById("questionsDiv");
+    let questionDiv = document.getElementById("questionsDiv");
     questionDiv.parentNode.removeChild(questionDiv);
+    let quizFormElement = document.getElementById("quizForm");
+    for (var quizQuestionIndex = 0; quizQuestionIndex < quizArray.length; quizQuestionIndex++) {
+      quizFormElement.insertAdjacentHTML("beforeend", "<div class='centerDiv'><span>" + quizArray[quizQuestionIndex] + "</span></div>");
+    }
   });
 });
