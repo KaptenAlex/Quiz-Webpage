65f3e9a6974007511c18a9934d99be24765e3d0b
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-09-25 11:45:05 +0200

    Print the amount of questions the user wants to answer.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  class Quiz{
-    constructor(){
+  class Quiz {
+    constructor() {
       this.userName = userName; //Should be a string
       this.noOfQuestions = noOfQuestions; //Should be an array.
       this.noOfRightAnswers = noOfRightAnswers; //Should be a number/array
       this.noOfWrongAnswers = noOfWrongAnswers; //Should be a number/array
     }
   }
-  class Question{
-    constructor(){
+  class Question {
+    constructor() {
       this.questionCategory = questionCategory; //Should be a string
       this.question = question; //Should be a string
       this.questionAnswers = questionAnswers; //Should be an array
@@ -19,11 +19,12 @@ document.addEventListener("DOMContentLoaded", () => {
   createQuizButtonElement = document.getElementById("btn_CreateQuiz");
   const quizArray = ["question 1", "question 2", "question 3", "question 4", "question 5", "question 6"];
   document.getElementById('btn_CreateQuiz').addEventListener("click", () => {
+    selectValue = document.getElementById("select_question").value;
     createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'><p>this is the form</p></div></div>");
     let questionDiv = document.getElementById("questionsDiv");
     questionDiv.parentNode.removeChild(questionDiv);
     let quizFormElement = document.getElementById("quizForm");
-    for (var quizQuestionIndex = 0; quizQuestionIndex < quizArray.length; quizQuestionIndex++) {
+    for (var quizQuestionIndex = 0; quizQuestionIndex < selectValue; quizQuestionIndex++) {
       quizFormElement.insertAdjacentHTML("beforeend", "<div class='centerDiv'><span>" + quizArray[quizQuestionIndex] + "</span></div>");
     }
   });
