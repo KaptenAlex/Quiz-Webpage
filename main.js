8d29fd9c9ddb61b9e5230c618380e202b1713ccb
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-09-25 10:13:55 +0200

    added div id, changed placeholder text and removes at click questionDiv.

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   createQuizButtonElement = document.getElementById("btn_CreateQuiz");
   document.getElementById('btn_CreateQuiz').addEventListener("click", () => {
     console.log("hej");
-    createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'><p>hello im a teapot</p></div></div>");
-    createQuizButtonElement.disabled = "disabled";
+    createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'><p>this is the form</p></div></div>");
+    var questionDiv = document.getElementById("questionsDiv");
+    questionDiv.parentNode.removeChild(questionDiv);
   });
 });
