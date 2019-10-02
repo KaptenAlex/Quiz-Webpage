ad9c5e05cd289386972a665ef0c6f88f10251a1c
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-10-02 12:48:24 +0200

    new function inside class Quiz

--- main.js
+++ main.js
@@ -5,41 +5,49 @@ document.addEventListener("DOMContentLoaded", () => {
   class Quiz {
     constructor() {
       this.userName = "";
-      this.noOfQuestions = Object.keys(json).length;
+      this.noOfQuestions = 0;
       this.noOfRightAnswers = 0;
       this.noOfWrongAnswers = 0;
+    }
+    loadQuiz(selectedQuestions) {
+      let createQuizDivElement = document.getElementById("createQuiz");
+      createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'></div></div>");
+      let quizFormElement = document.getElementById("quizForm");
+      //let question = new Question();
+      for (var i = 1; i <= selectedQuestions; i++) {
+        quizFormElement.insertAdjacentHTML("beforeend", "<div class='centerDiv'><span id='questionSpan'>" + "hej" + i + "</span><br></div>");
+        //let questionSpan = document.getElementById("questionSpan");
+        //for (var i = 0; i < Object.keys(json.); i++) {
+        //  questionSpan.insertAdjacentHTML("afterend", "<input type='radio' id='choice" + i + "'><label for='choice" + i + "'>" + );
+        //}
+      }
+      //insert radio/checkboxes after or at the same time as question span
     }
   }
   class Question {
     constructor() {
       this.questionCategory = ""; //Should be a string
-      this.question = ""; //Should be a string
+      this.question = "asdasd"; //Should be a string
       this.questionAnswers = []; //Should be an array
       this.isQuestionRightOrWrong = []; //Should be array with bool values
     }
   }
   const json = getJSON("http://demo3824117.mockable.io/");
-  console.log(json);
   let quiz = new Quiz();
-  createQuizDivElement = document.getElementById("createQuiz");
+  console.log(json);
   createQuizButtonElement = document.getElementById("btn_CreateQuiz");
   //const questionArray = ["Vem är djungelns konung?", "Vem är Zlatan", "Vad heter Elon Musks huvudföretag", "Vad är javascript?", "vad är ett objekt?"];
 
   select_question = document.getElementById("select_question");
-  for (var questionValue = 1; questionValue < quiz.noOfQuestions + 1; questionValue++) {
+  for (var questionValue = 1; questionValue < Object.keys(json).length + 1; questionValue++) {
     select_question.insertAdjacentHTML("beforeend", "<option>" + questionValue + "</option>");
   }
 
   document.getElementById('btn_CreateQuiz').addEventListener("click", () => {
     quiz.userName = document.getElementById("userNameInput").value;
-    let question = new Question();
-    selectValue = document.getElementById("select_question").value;
-    createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'></div></div>");
+    selectValue = Number(document.getElementById("select_question").value);
+    quiz.loadQuiz(selectValue);
     let questionDiv = document.getElementById("questionsDiv");
     questionDiv.parentNode.removeChild(questionDiv);
-    let quizFormElement = document.getElementById("quizForm");
-    for (var quizQuestionIndex = 0; quizQuestionIndex < selectValue; quizQuestionIndex++) {
-      quizFormElement.insertAdjacentHTML("beforeend", "<div class='centerDiv'><span>" + question.question[quizQuestionIndex] + "</span></div>");
-    }
   });
 });
