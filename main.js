99b64f06ea0797a71cca362b6ab35bf2182c6c5a
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-10-02 16:44:09 +0200

    prints out selectedValue * object.keys(json). needs fix

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   /*
     https://demo1976140.mockable.io/
   */
+  const json = getJSON("http://demo3824117.mockable.io/");
   class Quiz {
     constructor() {
       this.userName = "";
@@ -13,26 +14,54 @@ document.addEventListener("DOMContentLoaded", () => {
       let createQuizDivElement = document.getElementById("createQuiz");
       createQuizDivElement.insertAdjacentHTML("afterend", "<div class='centerDiv'><div id='quizForm' class='shapeForm'></div></div>");
       let quizFormElement = document.getElementById("quizForm");
-      //let question = new Question();
+      console.log(Object.keys(json));
       for (var i = 1; i <= selectedQuestions; i++) {
-        quizFormElement.insertAdjacentHTML("beforeend", "<div class='centerDiv'><span id='questionSpan'>" + "hej" + i + "</span><br></div>");
-        //let questionSpan = document.getElementById("questionSpan");
-        //for (var i = 0; i < Object.keys(json.); i++) {
-        //  questionSpan.insertAdjacentHTML("afterend", "<input type='radio' id='choice" + i + "'><label for='choice" + i + "'>" + );
-        //}
+        for (var questions in json) {
+          if (json.hasOwnProperty(questions)) {
+            let x = 0;
+            //<span id='questionSpan'>" + "hej" + i + "</span>
+            let breakRow = document.createElement("br");
+
+            let divForQuestion = document.createElement("div");
+            divForQuestion.id = "question" + i;
+            quizFormElement.appendChild(divForQuestion);
+
+            let labelForCategory = document.createElement("label");
+            labelForCategory.appendChild(document.createTextNode("Category: " + questions.category));
+            divForQuestion.appendChild(labelForCategory);
+            labelForCategory.insertAdjacentHTML("afterend", "<br>");
+
+            let labelForQuestion = document.createElement("label");
+            labelForQuestion.appendChild(document.createTextNode("Question: " + json.question1.question));
+            divForQuestion.appendChild(labelForQuestion);
+            labelForQuestion.insertAdjacentHTML("afterend", "<br>");
+            for (let choice of json.question1.choices) {
+              let checkboxForChoices = document.createElement("input");
+              checkboxForChoices.type = "checkbox";
+              checkboxForChoices.id = "answer" + x;
+              divForQuestion.appendChild(checkboxForChoices);
+
+              let labelForChoices = document.createElement("label");
+              labelForChoices.htmlFor = "answer" + x;
+              labelForChoices.appendChild(document.createTextNode(choice));
+              divForQuestion.appendChild(labelForChoices);
+              labelForChoices.insertAdjacentHTML("afterend", "<br>");
+              x++;
+              console.log(x);
+            }
+          }
+        }
       }
-      //insert radio/checkboxes after or at the same time as question span
     }
   }
   class Question {
     constructor() {
       this.questionCategory = ""; //Should be a string
-      this.question = "asdasd"; //Should be a string
+      this.question = "test"; //Should be a string
       this.questionAnswers = []; //Should be an array
       this.isQuestionRightOrWrong = []; //Should be array with bool values
     }
   }
-  const json = getJSON("http://demo3824117.mockable.io/");
   let quiz = new Quiz();
   console.log(json);
   createQuizButtonElement = document.getElementById("btn_CreateQuiz");
